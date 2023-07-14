7ea87b7cfd398c370c4c5d53b3ee214bc8e3446d
Sasa Ngwenya <ladysasa6@live.com>
2023-07-13 19:10:56 -0700

    Hello.puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
  *
- * Return: always 0
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
