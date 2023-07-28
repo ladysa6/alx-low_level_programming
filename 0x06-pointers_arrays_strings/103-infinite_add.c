7665ca521627800b63238a0b8b29f0bdcce9cc0f
Sasa Ngwenya <ladysasa6@live.com>
2023-07-27 19:13:38 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,90 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * infinite_add - Adds two numbers
- * @n1: first input string
- * @n2: second input string
- * @r: pointer to buffer where result is stored
- * @size_r: requested size for the buffer
- * Return: pointer to buffer where result is stored
- */
 
+/**
+ * infinite_add - adds two integers stored as strings
+ *
+ * @n1: first integer string to add
+ * @n2: second integer string to add
+ * @r: array to store resulting string in
+ * @size_r: size of array r
+ *
+ * Return: the summed string in r. If r is too small for the result,
+ * return 0;
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, n, len1, len2; /* i = iterator, n = carry over number */
+	int carry = 0, index = 0, index2;
+	char *s1 = n1, *s2 = n2;
 
-	for (len1 = 0; n1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; n2[len2] != '\0'; len2++)
-		;
-	if (size_r >= len1 || size_r >= len2)
-		if (len1 >= len2)
-			i = j = len2 - 1;
-		else
-			i = j = len1 - 1;
-	else
-		i = j = size_r;	
-	n = 0;
-	/* if first number >= 10, set the value to 1 and increase the buffer by 1.*/
-	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
+	while (*s1 != 0)
+		s1++;
+	while (*s2 != 0)
+		s2++;
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+	while (s2 != n2 - 1 && s1 != n1 - 1)
 	{
-		r[0] = 1 + '0';
-		j = 1;
-	}
-
-	while (i >= 0)
-	{
-		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
-		if (r[j] >= 10)
+		r[index] = *s2 - '0' + *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
 		{
-			r[j] %= 10;
-			n = 1;
+			carry++;
+			r[index] -= 10;
 		}
-		else
-			n = 0;
-		r[j] += '0';
-		i--;
-		j--;
+		index++;
+		s2--;
+		s1--;
+		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
+			return (0);
 	}
-	r[size_r] = '\0';
+	while (s1 != n1 - 1)
+	{
+		r[index] = *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s1--;
+		index++;
+		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
+			return (0);
+	}
+	while (s2 != n2 - 1)
+	{
+		r[index] = *s2 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s2--;
+		index++;
+		if (size_r == index && (s2 != n2 - 1 || carry == 1))
+			return (0);
+	}
+	if (carry == 1)
+	{
+		r[index] = '1';
+		r[index + 1] = 0;
+	}
+	else
+	{
+		r[index--] = 0;
+	}
+	index2 = 0;
+	while (index2 <= index)
+	{
+		carry = r[index];
+		r[index] = r[index2];
+		r[index2] = carry;
+		index--;
+		index2++;
+		}
 	return (r);
 }
