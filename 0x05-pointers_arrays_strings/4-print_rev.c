289da5776b13f8d8eaf02b6527ef1aa384ed47ce
Sasa Ngwenya <ladysasa6@live.com>
2023-08-15 04:24:42 -0700

    4. I can only go one way.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,26 @@
 #include "main.h"
+
 /**
- * print_rev - prints a string, followed by a new line
- * @s: input string to print
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, j, len;
+
+	i = 0;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	i--;
-	while (s[i] != '\0')
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[i--]);
+		_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }
